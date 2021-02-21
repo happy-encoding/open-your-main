3c3055c1753c588783ea90dd950a17f5be3ccdce
root <gaohan1130@163.com>
2021-02-21 23:49:50 +0800

    add LinkList

---
+++ 链表/link_list.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time : 2021/2/21 23:48
+# @Author : GaoHan
+# @Email : gaohan1130@163.com
+# @File : link_list.py
+# @Software: PyCharm
+class Node:
+    def __init__(self):
+        self.node = None
+    def add(self):
+        pass
+    def remove(self):
+        pass
+    
