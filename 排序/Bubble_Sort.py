aedad997c5ae5d2eb0f492bfbd1598523461c7c0
root <gaohan1130@163.com>
2021-02-21 23:36:37 +0800

    冒泡排序

---
+++ 排序/Bubble_Sort.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time : 2021/2/20 23:57
+# @Author : GaoHan
+# @Email : gaohan1130@163.com
+# @File : Bubble_Sort.py
+# @Software: PyCharm
+def bubble_sort(li01):
+    for i in range(len(li01)):
+        for x in (i, len(li01)-1):
+            if li01[i] < li01[x]:
+                li01[i], li01[x] = li01[x], li01[i]
+    return li01
+
+
+print(bubble_sort([2, 41, 34, 3, 2, 33, 4, 5, 8, 12]))
+
