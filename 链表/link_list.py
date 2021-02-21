fd7476085b9d998a2542fc677e804291e3bb228b
root <gaohan1130@163.com>
2021-02-22 00:14:53 +0800

    modify LinkList

--- 链表/link_list.py
+++ 链表/link_list.py
@@ -5,16 +5,43 @@
 # @Email : gaohan1130@163.com
 # @File : link_list.py
 # @Software: PyCharm
+"""
+python列表实现单链表
+"""
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
 
     def add(self, item):
+        """
+        在链表头部增加节点
+        :param item: 节点对象
+        :return: None
+        """
         pass
 
     def append(self, item):
+        """
+        在链表尾部增加节点
+        :param item: 需要增加的节点对象
+        :return: None
+        """
         pass
 
     def remove(self):
+        pass
+
+    def is_empty(self):
+        """
+        判断链表是否为空
+        :return: None
+        """
+        pass
+
+    def len(self):
+        """
+        判断链表长度
+        :return: 链表长度
+        """
         pass
