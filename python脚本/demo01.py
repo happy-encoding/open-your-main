8d73a0f80c1bbb89aee9b0531d97202723c4d11f
root <gaohan1130@163.com>
2021-02-23 23:35:12 +0800

    add python脚本

---
+++ python脚本/demo01.py
@@ -0,0 +1,8 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time : 2021/2/23 23:34
+# @Author : GaoHan
+# @Email : gaohan1130@163.com
+# @File : demo01.py
+# @Software: PyCharm
+import os, sys
