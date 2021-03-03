62be64dc2c9f887f9666c002b0ced8f6f0ccbb7f
root <gaohan1130@163.com>
2021-03-03 23:21:14 +0800

    sys.exit()

--- python脚本/demo02.py
+++ python脚本/demo02.py
@@ -6,7 +6,19 @@
 # @File : demo02.py
 # @Software: PyCharm
 import sys
+
 # 获取参数  （列表）
 print(sys.argv)
 # 返回模块可以搜索到的环境变量
 print(sys.path)
+# 添加环境变量
+# sys.path.append("BASH_DIR")
+print(sys.path)
+# 获取当前平台（win，linux，unix）
+print(sys.platform)
+
+# sys.exit() 异常退出
+for i in range(5):
+    if i ==3:
+        sys.exit(3)
+    print(i)
