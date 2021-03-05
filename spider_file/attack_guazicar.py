2b5e8eee2bdec3f6783610e4aea1b22982180a4f
root <gaohan1130@163.com>
2021-03-05 23:56:42 +0800

    add User-Agent

--- spider_file/attack_guazicar.py
+++ spider_file/attack_guazicar.py
@@ -7,6 +7,8 @@
 # @Software: PyCharm
 import requests
 url = "https://www.guazi.com/wh/"
-resp= requests.get(url=url)
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1"}
+resp= requests.get(url=url,headers=headers)
 html = resp.content
 print(html)
--- spider_file/headers_test.py
+++ spider_file/headers_test.py
@@ -11,5 +11,7 @@
 import requests
 
 url = "http://httpbin.org/get"
-html = requests.get(url).text
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1"}
+html = requests.get(url=url, headers=headers).text
 print(html)
